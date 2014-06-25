8eded76233bee98e7a0f9a40bc3141879e923186
L Kodai <lauren@laurenkodai.com>
2014-06-25 16:07:03 -0700

    Actual content initial commit

---
+++ Readme.md
@@ -0,0 +1,37 @@
+# This needs updated
+
+Sorry bout it
+
+
+<!-- ## Setup
+
+Install [Sass v3.3](http://sass-lang.com/install)
+
+    gem install sass
+
+You may need to run `sudo gem install sass`.
+
+You will need [node](http://nodejs.org/). Then install bower and gulp globally
+
+    npm install -g bower gulp
+
+ _You may need to use `sudo`._
+
+ Next - drop into the nxsass dir and run:
+
+    npm install -->
+
+<!-- ### Gulp Tasks
+
+Default Gulp task builds sass and kicks off server on port `1337` with Livereload:
+
+    gulp
+
+Compile Sass:
+
+    gulp sass
+
+Run `watch` with LiveReload [You will need the browser plugin](https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei?hl=en)
+
+    gulp watch -->
+
---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    fileinclude = require('gulp-file-include'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify'),
+    livereload = require('gulp-livereload'),
+    lr = require('tiny-lr'),
+    connect = require('gulp-connect'),
+    server = lr();
+
+
+// fileinclude: grab partials and render single html file
+// ==========================================
+// gulp.task('fileinclude', function() {
+//   return  gulp.src(['./partials/index.tpl.html'])
+//     .pipe(rename("index.html"))
+//     .pipe(fileinclude())
+//     .pipe(gulp.dest('./'))
+//     .pipe(livereload(server))
+//     .pipe(notify({ message: 'Includes: included' }));
+// });
+gulp.task('fileinclude', function() {
+  gulp.src(['./tpl/*.tpl.html'])
+    
+    .pipe(fileinclude())
+    .pipe(rename({
+        extname: ""
+      }))
+    .pipe(rename({
+        extname: ".html"
+      }))
+    .pipe(gulp.dest('./out/'))
+    .pipe(livereload(server))
+    .pipe(notify({ message: 'Includes: included' }));
+});
+//  Sass: compile sass to css task
+//===========================================
+gulp.task('sass', function() {
+  return gulp.src('css/source/app.scss')
+    .pipe(sass({ style: 'expanded', errLogToConsole: true}))
+    .pipe(gulp.dest('css'))
+    .pipe(livereload(server))
+    .pipe(notify({ message: 'SO Sassy!' }));
+});
+
+
+//  Connect: sever task
+//===========================================
+gulp.task('connect', connect.server({
+  port: 1337,
+  root: [__dirname],
+  livereload: false
+}));
+
+
+//  Watch and Livereload
+//===========================================
+gulp.task('watch', function() {
+
+  // Listen on port 35729
+  server.listen(35729, function (err) {
+    if (err) {
+      return console.error(err) 
+      //TODO use notify to log a message on Sass compile fail and Beep
+    };
+
+    //Watch task for sass
+    gulp.watch('css/source/**/*.scss', ['sass']);
+
+    // watch task for gulp-includes
+    gulp.watch('partials/**/*.html', ['fileinclude']);
+    gulp.watch('tlp/*.tpl.html');
+
+  });
+
+});
+
+
+//  Default Gulp Task
+//===========================================
+gulp.task('default', ['fileinclude', 'sass', 'connect', 'watch'], function() {
+
+});
+
+
+
